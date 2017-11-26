3e4a4c412cd16fb65ed7c9cf7d0a8e64d2edfe34
Miraclx <omiraculous@gmail.com>
2017-11-26 12:39:51 +0100

    Pretty Printed...Thats better

--- src/js/greeter.js
+++ src/js/greeter.js
@@ -9,10 +9,11 @@
 var active_users = 0;
 if (!('lightdm' in window)) {
   try {
-    var lightdmMock = lightdmMock || {
-    };
+    var lightdmMock = lightdmMock || {};
     window.lightdm = new lightdmMock(true, 0, false);
-    if ('lightdm' in window) console.log('LightDM Mock initialised', 'lightdm');
+    if ('lightdm' in window) {
+      console.log('LightDM Mock initialised', 'lightdm');
+    }
   } catch (err) {
     console.error('LightDM Mock failed to initialise');
   }
@@ -29,32 +30,34 @@ window.show_prompt = (prompt, type='password') => {
   }, 250);
   $('#pass_entry').attr('type', type);
 }
-window.show_message = (msg, type='info') => {
+window.show_message = (msg,type='info') => {
   opts = {};
   switch (type) {
-    case 'error':
-      type = 'danger';
-      opts = {
-        placement: {
-          from: "bottom",
-          align: "left"
-        }
-      };
-      break;
-    case 'warning':
-      attr = {
-        icon: 'fa fa-bell-o'
+  case 'error':
+    type = 'danger';
+    opts = {
+      placement: {
+        from: "bottom",
+        align: "left"
       }
-      break;
-    case 'success':
-      attr = {
-        icon: 'fa fa-unlock'
-      }
-      break;
+    };
+    break;
+  case 'warning':
+    attr = {
+      icon: 'fa fa-bell-o'
+    }
+    break;
+  case 'success':
+    attr = {
+      icon: 'fa fa-unlock'
+    }
+    break;
   }
   console.log(typeof attr == "undefined");
   if (typeof attr != "undefined") {
-    opts = mixIn(opts, { type: type } );
+    opts = mixIn(opts, {
+      type: type
+    });
   } else {
     attr = type;
   }
@@ -83,8 +86,8 @@ window.authentication_complete = () => {
 
 //Personal Functions
 function user_clicked(event) {
-  if ( event.target.getAttribute('id') == data.selected_user ) {
-    show_message("Already authenticating "+ getPack(data.selected_user).display_name, 'warning');
+  if (event.target.getAttribute('id') == data.selected_user) {
+    show_message("Already authenticating " + getPack(data.selected_user).display_name, 'warning');
   }
   if (lightdm.in_authentication) {
     lightdm.cancel_authentication();
@@ -105,7 +108,7 @@ function init() {
     //Init the user list
     initUsers();
   } catch (exception) {
-    console.error('Error: \n'+exception);
+    console.error('Error: \n' + exception);
   }
 }
 function initUsers() {
@@ -117,13 +120,13 @@ function initUsers() {
     $userNode = $name_template.clone();
     $children = $name_template.children().clone(true, true);
     dispName = selected_user.display_name;
-    $userNode.html( c$().reduce(15, dispName) );
-    $children.each(function () {
+    $userNode.html(c$().reduce(15, dispName));
+    $children.each(function() {
       $userNode.append(this);
     });
     if (selected_user.logged_in) {
       ++active_users;
-      $($userNode).find('.user_locked').each(function () {
+      $($userNode).find('.user_locked').each(function() {
         $(this)
           .addClass('fa fa-circle')
           .css({
@@ -134,7 +137,7 @@ function initUsers() {
             'padding-top': '18px',
             'color': '#33D900'
           });
-      });
+        });
     }
     // Implement the trial count
     $userNode.attr('id', selected_user.username);
@@ -150,20 +153,20 @@ function setUserImage(user, box) {
   if (box.attr('rpath') != user.image) {
     $('#user_image').fadeOut(250, () => {
       if (user.image) {
-    box.attr('src', user.image);
-    box.attr('rpath', user.image);
-    box.on('error', () => {
-      box.attr('src', 'src/img/avatar/default.png');
-    });
+        box.attr('src', user.image);
+        box.attr('rpath', user.image);
+        box.on('error', () => {
+          box.attr('src', 'src/img/avatar/default.png');
+        });
       } else {
-    box.attr('src', 'src/img/avatars/default.png');
+        box.attr('src', 'src/img/avatars/default.png');
       }
     }).fadeIn(250);
   }
 }
 function authUser(user) {
   data.selected_user = user;
-  data.session = $("#"+user).attr('session');
+  data.session = $("#" + user).attr('session');
   setUserImage(getPack(user), $('#user_image'));
   if (user) {
     lightdm.authenticate(user);
@@ -171,11 +174,11 @@ function authUser(user) {
 }
 function setTheme(theme) {
   box = window.themes[theme];
-  if ( box.font ) {
+  if (box.font) {
     $("*:not(.fa)").css("font-family", box.font);
   }
   if (box.container_radius) {
-    $("#login_container").css("border-radius", box.container_radius );
+    $("#login_container").css("border-radius", box.container_radius);
   }
   if (box.user_image_size) {
     $("#user_image").css("width", box.user_image_size);
@@ -194,11 +197,11 @@ function getPack(username) {
       return lightdm.users[i];
     }
   }
-} 
+}
 function prepShoot() {
   splash_notify = notify('Hello There, Preparing...', {
-    icon: 'fa fa-spin fa-spinner' 
-    }, {
+    icon: 'fa fa-spin fa-spinner'
+  }, {
     type: 'warning',
     showProgressbar: true,
     delay: 4000,
@@ -210,17 +213,17 @@ function prepShoot() {
   });
   $('select, .user_selector').niceSelect();
   FastClick.attach(document.body);
-  setTimeout( () => {
+  setTimeout(() => {
     splash_notify.update({
       message: "Preparing plug-in's",
     });
   }, 1500);
-  setTimeout( () => {
+  setTimeout(() => {
     splash_notify.update({
       message: "Gathering Users",
     });
   }, 2200);
-  setTimeout( () => {
+  setTimeout(() => {
     splash_notify.update({
       message: "Applying User configurations",
       type: "info"
@@ -228,7 +231,7 @@ function prepShoot() {
   }, 3000);
   // ON READY MODULES
   $(document).ready(() => {
-    setTimeout( () => {
+    setTimeout(() => {
       splash_notify.update({
         message: "All set",
         type: "success"
@@ -238,13 +241,13 @@ function prepShoot() {
       $("#loading").css('opacity', '0');
       setTimeout(() => {
         $("#loading").hide();
-      },1000);
+      }, 1000);
     }, 4000);
   });
 }
 
 function notify(msg='', ...args) {
-  var type = 'info', attr = {}, options = {};
+  var type = 'info', attr = options = {};
   if (args[0] && typeof args[0] === 'string') {
     type = args[0];
   } else if (args[0] && typeof args[0] === 'object') {
@@ -253,21 +256,19 @@ function notify(msg='', ...args) {
   if (args[1] && typeof args[1] === 'object') {
     options = args[1];
   }
-  return $.notify(
-  mixIn({
+  return $.notify(mixIn({
     message: msg,
     title: "ArtanOS Lock",
     icon: "fa fa-bell"
-  }, attr),
-  mixIn({
+  }, attr), mixIn({
     type: type,
     newest_on_top: true,
     z_index: 11,
     delay: 2000,
     timer: 500,
     animate: {
-      enter: 'animated '+((type=='danger')? 'flipInY' : 'bounceInDown'),
-      exit:  'animated '+((type=='danger')? 'flipOutX' : 'bounceOutUp')
+      enter: 'animated ' + ((type == 'danger') ? 'flipInY' : 'bounceInDown'),
+      exit: 'animated ' + ((type == 'danger') ? 'flipOutX' : 'bounceOutUp')
     },
     template: '\
     <div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert">\
@@ -285,12 +286,12 @@ function notify(msg='', ...args) {
   }, options));
 }
 
-function mixIn( orig, other ) {
+function mixIn(orig, other) {
   final = {};
-  for ( key in orig ) {
+  for (key in orig) {
     final[key] = orig[key];
   }
-  for ( key in other ) {
+  for (key in other) {
     final[key] = other[key];
   }
   return final;
@@ -304,7 +305,7 @@ function initFPB() {
     }
   }
   function toggleLmtr() {
-    if ( $("#powerLmtr").is(":visible") ) {
+    if ($("#powerLmtr").is(":visible")) {
       $("#powerLmtr").hide();
     } else {
       $("#powerLmtr").show();
@@ -328,15 +329,15 @@ function initFPB() {
     } else if (btn.hasClass('power-btn-restart')) {
       card.css('background-color', '#388e3c');
     }
-    $('#power-yes')[0].onclick = function () {
+    $('#power-yes')[0].onclick = function() {
       eval(btn.parent().attr('action'));
     };
   });
 }
 
 function initTooltip() {
-  $('[tooltip]').hover(function (evt) {
-    ordin8 = getCoord( evt.currentTarget, 'top', 'left' );
+  $('[tooltip]').hover(function(evt) {
+    ordin8 = getCoord(evt.currentTarget, 'top', 'left');
     $that = $(evt.currentTarget);
     tooltip = $that.attr('tooltip');
     display_text = forMatr(tooltip, {
@@ -345,42 +346,42 @@ function initTooltip() {
       '%id': $that.attr('id')
     });
     $('#tooltip-box')
-      .html( display_text )
-      .css('left', (ordin8.left)+'px')
-      .css('top', (ordin8.top+$that.outerHeight())+'px')
+      .html(display_text)
+      .css('left', (ordin8.left) + 'px')
+      .css('top', (ordin8.top + $that.outerHeight()) + 'px')
       .show();
-  }, function () {
+  }, function() {
     $('#tooltip-box').hide();
   });
 }
 
 function getCoord(el, ...direction) {
   function get(el, pos) {
-    var scrollTop     = $(window).scrollTop(),
-    elementOffset = $(el).offset()[pos];
+    var scrollTop = $(window).scrollTop(),
+        elementOffset = $(el).offset()[pos];
     return (elementOffset - scrollTop);
   }
   if (direction.length > 1) {
     res = {};
-    for (i = 0; i < direction.length; i++ ) {
-      res[direction[i]] = get( el, direction[i] );
+    for (i = 0; i < direction.length; i++) {
+      res[direction[i]] = get(el, direction[i]);
     }
   } else {
-    res = get( el, direction[0] );
+    res = get(el, direction[0]);
   }
   return res;
 }
 
 function forMatr(txt, ...format) {
-  if ( format.length == 1 && typeof format[0] === 'object' ) {
+  if (format.length == 1 && typeof format[0] === 'object') {
     dict = format[0];
-    for ( i in dict ) {
+    for (i in dict) {
       txt = txt.replace(i, dict[i]);
     }
   } else if (typeof format[0] === 'string' && typeof format[1] === 'string') {
     txt = txt.replace(format[0], format[1]);
   } else {
-    console.error('Illegal formatting');
+    console.error('forMatr:: Illegal formatting');
   }
   return txt;
 }
